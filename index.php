1c5af074176e75a1246243a11dfa4ebd4dd15a5c
Pedro <111528573+Fokinha@users.noreply.github.com>
2023-02-27 16:44:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php echo "^-^ Olas Mundo ^-^";
+     ?>
+<h1>
+    Operacoes Matematicas:
+</h1>
+<h2>3-90=
+    <?php echo(90-3);
+?>
+</h2>
+<h2>79+340=
+    <?php echo(79+340);
+?>
+
+</h2>
+<h2>80/4=
+    <?php echo(80/4);
+?>
+</h2>
+<h2>5x46=
+    <?php echo(5*46);
+?>
+</h2>
+</body>
+</html>
